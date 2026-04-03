630874877f8b1355697f8caccfb24a59f8850e98
Bluntest64 <123kevindavidgomezposada@gmail.com>
2026-04-03 14:31:23 -0500

    replace replace

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import psycopg2
 import os
+from psycopg2.extras import RealDictCursor
 
 def get_connection():
-    return psycopg2.connect(os.environ.get("DATABASE_URL"))
+    return psycopg2.connect(os.environ.get("DATABASE_URL"),
+        cursor_factory=RealDictCursor)
