7235c58c770ae7cd743380ccf8ff65286005724f
Bluntest64 <123kevindavidgomezposada@gmail.com>
2026-04-03 03:01:26 -0500

    change URL EXTERNAL > DATABASE_URL

--- database.py
+++ database.py
@@ -2,4 +2,4 @@ import psycopg2
 import os
 
 def get_connection():
-    return psycopg2.connect(os.environ.get("postgresql://usuario:tDDjysXCnrQD3dT9FkvQU3vogISl9pLb@dpg-d76u8via214c73d5o4ig-a.oregon-postgres.render.com/database_5rjq"))
+    return psycopg2.connect(os.environ.get("DATABASE_URL"))
